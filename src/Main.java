a385a5acf21c40d3db0e69c99f92c9f98946130d
Maryam Chertmanova <mchertmanova@gmail.com>
2020-11-10 12:49:28 -0500

    project shared inn already in remote repo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         //B21 is good at InteliJ
-        //Maac user and windows are happy now
+        //Mac user and windows are happy now
+        //I have already publish on GitHub .com
+        //but do i need to share again?
+        //time to commit and push
     }
 }
