7e6e4d01643ff3271e73e67ba02af58ca617d2b5
Maryam Chertmanova <mchertmanova@gmail.com>
2020-11-10 12:26:03 -0500

    initial commit added note

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         //B21 is good at InteliJ
+        //Maac user and windows are happy now
     }
 }
